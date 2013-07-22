7f0e55abea251477bede40677e856eadc93abfef
Eric George <eric@pokelondon.com>
2013-07-22 18:14:47 +0100

    Rambo images

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     var images = [
-        'http://rack.2.mshcdn.com/media/ZgkyMDEzLzAyLzI4L2U4L3Nob3dlcmNhdC44ZGZiZi5naWYKcAl0aHVtYgkxMjAweDk2MDA-/7c27db65/bd5/shower-cat.gif',
-        'http://25.media.tumblr.com/tumblr_mbaic7FGXT1qbvq2so1_500.gif',
-        'http://www.reactiongifs.com/wp-content/uploads/2013/05/deal.gif',
-        'http://gifling.com/galleries/gifs1/cat-digging-snow.gif',
-        'http://24.media.tumblr.com/tumblr_m0py8284X31qf5z7ko1_500.gif',
-        'http://d1d2e8z52bbsdm.cloudfront.net/blog/wp-content/uploads/2013/05/cat-bath.gif'
+        '../images/1.gif',
+        '../images/2.gif',
+        '../images/3.gif',
+        '../images/4.gif',
+        '../images/5.gif',
+        '../images/6.gif'
     ];
 
     $stage = $('#stage');
